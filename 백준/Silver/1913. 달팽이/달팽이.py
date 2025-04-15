69219387fa330ed96dd62e1ded27a79c36a801de
devkim927 <dimsiul6@gmail.com>
2025-04-16 00:13:57 +0900

    [Silver III] Title: 달팽이, Time: 1104 ms, Memory: 70928 KB -BaekjoonHub

---
+++ 백준/Silver/1913. 달팽이/달팽이.py
@@ -0,0 +1,30 @@
+N = int(input())
+M = int(input())
+start = N*N
+grid = [[False]*N for _ in range(N)]
+
+dy = [0, 1, 0, -1]
+dx = [1, 0, -1, 0]
+i, j, dr = 0, 0, 0
+
+M_spot = [0, 0]
+
+grid[i][j] = start
+start -= 1
+if grid[i][j] == M:
+    M_spot[0], M_spot[1] = i+1, j+1
+
+while start > 0:
+    ni, nj = i+dx[dr], j+dy[dr]
+    if 0 <= ni < N and 0 <= nj < N and grid[ni][nj] == False:
+        i, j = ni, nj
+        grid[i][j] = start
+        start -= 1
+    else:
+        dr = (dr+1)%4
+    if grid[i][j] == M:
+        M_spot[0], M_spot[1] = i+1, j+1
+        
+for i in grid:
+    print(*i)
+print(*M_spot)
